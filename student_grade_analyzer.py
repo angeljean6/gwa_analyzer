3b1fa1c1fda737a29e5a76b1c40bdb584e77e8a4
angeljean6 <ajrescober@gmail.com>
2026-04-29 20:35:59 +0800

    added find_top_student method and file reader

--- student_grade_analyzer.py
+++ student_grade_analyzer.py
@@ -2,5 +2,8 @@ class StudentAnalyzer:
     def __init__(self, data_file):
         self.data_file = data_file
 
-
+    def find_top_student(self):
+        """Identifies the student with the lowest GWA (highest achievement)."""
+        best_student = ""
+        best_gwa = float('inf')
 
