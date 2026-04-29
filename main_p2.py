072f05ec2561bb11897a0c6abe9df9b91ca8a085
angeljean6 <ajrescober@gmail.com>
2026-04-29 20:58:56 +0800

    added import for studentanalyzer class

---
+++ main_p2.py
@@ -0,0 +1,11 @@
+from student_grade_analyzer import StudentAnalyzer
+
+def run_analysis():
+    analyzer = StudentAnalyzer("students.txt")
+
+    print("Analyzing student records...")
+    analyzer.find_top_student()
+
+
+if __name__ == "__main__":
+    run_analysis()
