cf0cdbdb9c034d40abe08be43062764bfc15b6cb
angeljean6 <ajrescober@gmail.com>
2026-04-29 21:04:55 +0800

    added call to find_top_student with status print

--- main_p2.py
+++ main_p2.py
@@ -3,4 +3,6 @@ from student_grade_analyzer import StudentAnalyzer
 def run_analysis():
 
     analyzer = StudentAnalyzer("students.txt")
-    
+
+    print("Analyzing student records...")
+    analyzer.find_top_student()
