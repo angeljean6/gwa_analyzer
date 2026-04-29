531df32921da317e2fdaeec3bb6d6937f75bc8c7
angeljean6 <ajrescober@gmail.com>
2026-04-29 21:03:15 +0800

    added run_analysis function to initialize analyzer

--- main_p2.py
+++ main_p2.py
@@ -1,11 +1,6 @@
 from student_grade_analyzer import StudentAnalyzer
 
 def run_analysis():
+
     analyzer = StudentAnalyzer("students.txt")
-
-    print("Analyzing student records...")
-    analyzer.find_top_student()
-
-
-if __name__ == "__main__":
-    run_analysis()
+    
